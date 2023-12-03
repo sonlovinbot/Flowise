6958178c47a88b74b52ba216db8a53473b6bd01b
LovinBot <129473737+sonlovinbot@users.noreply.github.com>
2023-12-04 02:57:38 +0700

    Update Logo.js

--- packages/ui/src/ui-component/extended/Logo.js
+++ packages/ui/src/ui-component/extended/Logo.js
@@ -1,5 +1,5 @@
-import logo from 'assets/images/logo-lovinbot-white.png'
-import logoDark from 'assets/images/logo-lovinbot-dark.png'
+import logo from 'assets/images/flowise_logo.png'
+import logoDark from 'assets/images/flowise_logo_dark.png'
 
 import { useSelector } from 'react-redux'
 
